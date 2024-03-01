2933ad8a3ce1dc2b44ac6d32e568a4ba1465df43
Ryan Ali <ryanali456@gmail.com>
2024-03-01 22:40:10 +0800

    create driver controller and initial driver page

---
+++ DeliveryTrackingApp/Areas/Admin/Controllers/DriverController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryTrackingApp.Areas.Admin.Controllers
+{   
+    [Area("Admin")]
+    public class DriverController : Controller
+    {
+        // GET: DriverController
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult New(){
+            return View();
+        }
+
+    }
+}
