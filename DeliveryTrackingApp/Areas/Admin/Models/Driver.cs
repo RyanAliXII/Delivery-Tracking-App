b4ba4c1cd012e30e4358b022e7b8353fac4f5223
Ryan Ali <ryanali456@gmail.com>
2024-03-02 00:20:36 +0800

    add default value to string field of driver model

--- DeliveryTrackingApp/Areas/Admin/Models/Driver.cs
+++ DeliveryTrackingApp/Areas/Admin/Models/Driver.cs
@@ -1,6 +1,7 @@
 
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DeliveryTrackingApp.Areas.Admin.Models;
 
@@ -8,27 +9,34 @@ public class Driver {
     
     public Guid Id {get; set;}
     [Required(ErrorMessage = "Given name is required.")]
-    public string GivenName {get; set;}
+    public string GivenName {get; set;} = "";
     [Required(ErrorMessage = "Middle name is required.")]
-    public string MiddleName {get; set;}
+    public string MiddleName {get; set;} = "";
     [Required(ErrorMessage = "Surname is required.")]
-    public string Surname {get; set;}
+    public string Surname {get; set;} = "";
     [DataType(DataType.Date)]
     [Required(ErrorMessage = "Date of birth is required.")]
     public DateTime DateOfBirth {get; set;}
     [Required(ErrorMessage = "Gender is required.")]
     
-    public string Gender {get; set;}
+    public string Gender {get; set;} = "";
     [Required(ErrorMessage = "License ID number is required.")]
-    public string LicenseIdNumber {get; set;}
+    public string LicenseIdNumber {get; set;} = "";
      [DataType(DataType.Date)]
      [Required(ErrorMessage = "License validity is required.")]
     public DateTime LicenseValidity {get; set;}
 
     // public string LicensePhoto  {get;set;}
-    [Required(ErrorMessage = "Email is required.")]
+    [Required(ErrorMessage = "Email is required.")] 
     
-    public string Email {get; set;}
+    public string Email {get; set;} = "";
     [Required(ErrorMessage = "Mobile number is required.")]
-    public string MobileNumber {get; set;}
+    public string MobileNumber {get; set;} = "";
+
+    public List<SelectListItem> Genders { get; set;} =
+    [
+            new SelectListItem { Value = "Male", Text = "Male" },
+            new SelectListItem { Value = "Female", Text = "Female" },
+            new SelectListItem { Value = "Other", Text = "Other"  },
+    ];
 }
