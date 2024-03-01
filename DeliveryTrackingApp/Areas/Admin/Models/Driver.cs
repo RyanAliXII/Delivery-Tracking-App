8a32e24495187f663b1e71b49a07b4e6fc442a50
Ryan Ali <ryanali456@gmail.com>
2024-03-01 23:55:14 +0800

    add custom error validation message

--- DeliveryTrackingApp/Areas/Admin/Models/Driver.cs
+++ DeliveryTrackingApp/Areas/Admin/Models/Driver.cs
@@ -7,17 +7,28 @@ namespace DeliveryTrackingApp.Areas.Admin.Models;
 public class Driver {
     
     public Guid Id {get; set;}
+    [Required(ErrorMessage = "Given name is required.")]
     public string GivenName {get; set;}
+    [Required(ErrorMessage = "Middle name is required.")]
     public string MiddleName {get; set;}
+    [Required(ErrorMessage = "Surname is required.")]
     public string Surname {get; set;}
     [DataType(DataType.Date)]
+    [Required(ErrorMessage = "Date of birth is required.")]
     public DateTime DateOfBirth {get; set;}
+    [Required(ErrorMessage = "Gender is required.")]
+    
     public string Gender {get; set;}
+    [Required(ErrorMessage = "License ID number is required.")]
     public string LicenseIdNumber {get; set;}
      [DataType(DataType.Date)]
+     [Required(ErrorMessage = "License validity is required.")]
     public DateTime LicenseValidity {get; set;}
 
     // public string LicensePhoto  {get;set;}
+    [Required(ErrorMessage = "Email is required.")]
+    
     public string Email {get; set;}
+    [Required(ErrorMessage = "Mobile number is required.")]
     public string MobileNumber {get; set;}
 }
