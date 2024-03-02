9b27a6323c33a9438185a6fad4b2a9fa2b3d0641
Ryan Ali <ryanali456@gmail.com>
2024-03-02 20:21:09 +0800

    format driver viewmodel class, remove spaces

--- DeliveryTrackingApp/Areas/Admin/ViewModels/DriverViewModel.cs
+++ DeliveryTrackingApp/Areas/Admin/ViewModels/DriverViewModel.cs
@@ -4,28 +4,18 @@ using DeliveryTrackingApp.Areas.Admin.Models;
 namespace DeliveryTrackingApp.Areas.Admin.ViewModels;
 public class DriverViewModel { 
     public Guid Id {get; set;}
-   
     public string GivenName {get; set;} = "";
-   
     public string MiddleName {get; set;} = "";
-   
     public string Surname {get; set;} = "";
- 
     public DateTime DateOfBirth {get; set;} = DateTime.Now;
- 
     public string Gender {get; set;} = "";
-  
     public string LicenseIdNumber {get; set;} = "";
-    
     public DateTime LicenseValidity {get; set;} = DateTime.Now;
-
     public string LicenseImagePath  {get;set;} = "";
     public string Address {get;set;} = "";
     public string MobileNumber {get; set;} = "";
     public string Email {get; set;} = "";
-
     public DriverViewModel(){}
-
     public DriverViewModel(Driver d){
         Id = d.Id;
         GivenName = d.GivenName;
