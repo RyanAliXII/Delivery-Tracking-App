2e900f553c867d7cecd2c0c2b27b2f68557ef641
Ryan Ali <ryanali456@gmail.com>
2024-03-02 12:36:05 +0800

    implement unitofwork class

--- DeliveryTrackingApp/Repositories/UnitOfWork.cs
+++ DeliveryTrackingApp/Repositories/UnitOfWork.cs
@@ -1,5 +1,16 @@
+using DeliveryTrackingApp.Data;
+
 namespace DeliveryTrackingApp.Repositories;
 
-public class UnitOfWork {
-        
+public class UnitOfWork(DefaultDbContext dbContext) : IUnitOfWork {
+    private readonly DefaultDbContext _dbContext = dbContext;
+    public IDriverRepository DriverRepository { get; private set; } = new DriverRepository(dbContext);
+
+    public void Save(){
+            _dbContext.SaveChanges();
+    }
+}
+
+public interface IUnitOfWork {
+    public IDriverRepository DriverRepository {get;}
 }
