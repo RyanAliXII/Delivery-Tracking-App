f325a0ef20647b4ef97d5055c804bb715d007b54
Ryan Ali <ryanali456@gmail.com>
2024-03-04 15:23:00 +0800

    add string.Empty

--- DeliveryTrackingApp/Areas/Admin/ViewModels/DriverViewModel.cs
+++ DeliveryTrackingApp/Areas/Admin/ViewModels/DriverViewModel.cs
@@ -5,17 +5,17 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace DeliveryTrackingApp.Areas.Admin.ViewModels;
 public class DriverViewModel { 
     public Guid Id {get; set;}
-    public string GivenName {get; set;} = "";
-    public string MiddleName {get; set;} = "";
-    public string Surname {get; set;} = "";
+    public string GivenName {get; set;} = string.Empty;
+    public string MiddleName {get; set;} = string.Empty;
+    public string Surname {get; set;} = string.Empty;
     public DateTime DateOfBirth {get; set;} = DateTime.Now;
-    public string Gender {get; set;} = "";
-    public string LicenseIdNumber {get; set;} = "";
+    public string Gender {get; set;} = string.Empty;
+    public string LicenseIdNumber {get; set;} = string.Empty;
     public DateTime LicenseValidity {get; set;} = DateTime.Now;
-    public string LicenseImagePath  {get;set;} = "";
-    public string Address {get;set;} = "";
-    public string MobileNumber {get; set;} = "";
-    public string Email {get; set;} = "";
+    public string LicenseImagePath  {get;set;} = string.Empty;
+    public string Address {get;set;} = string.Empty;
+    public string MobileNumber {get; set;} = string.Empty;
+    public string Email {get; set;} = string.Empty;
     public DriverViewModel(){}
     public DriverViewModel(Driver d){
         Id = d.Id;
@@ -36,28 +36,28 @@ public class DriverViewModel {
 public class NewDriverViewModel { 
     public Guid Id {get; set;}
     [Required(ErrorMessage = "Given name is required.")]
-    public string GivenName {get; set;} = "";
+    public string GivenName {get; set;} = string.Empty;
     [Required(ErrorMessage = "Middle name is required.")]
-    public string MiddleName {get; set;} = "";
+    public string MiddleName {get; set;} = string.Empty;
     [Required(ErrorMessage = "Surname is required.")]
-    public string Surname {get; set;} = "";
+    public string Surname {get; set;} = string.Empty;
     [DataType(DataType.Date)]
     [Required(ErrorMessage = "Date of birth is required.")]
     public DateTime DateOfBirth {get; set;} = DateTime.Now;
     [Required(ErrorMessage = "Gender is required.")]
-    public string Gender {get; set;} = "";
+    public string Gender {get; set;} = string.Empty;
     [Required(ErrorMessage = "License ID number is required.")]
-    public string LicenseIdNumber {get; set;} = "";
+    public string LicenseIdNumber {get; set;} = string.Empty;
     [DataType(DataType.Date)]
     [Required(ErrorMessage = "License validity is required.")]
     public DateTime LicenseValidity {get; set;} = DateTime.Now;
     [Required(ErrorMessage = "License image is required.")]
     public IFormFile? LicenseImage {get; set;}
-    public string LicenseImagePath  {get;set;} = "";
-    public string Address {get;set;} = "";
+    public string LicenseImagePath  {get;set;} = string.Empty;
+    public string Address {get;set;} = string.Empty;
    
     [Required(ErrorMessage = "Mobile number is required.")]
-    public string MobileNumber {get; set;} = "";
+    public string MobileNumber {get; set;} = string.Empty;
     
     [ForeignKey("AccountId")]
     public virtual Account Account {get;set;} = new Account();
