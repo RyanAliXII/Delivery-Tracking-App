a30b09536c8e20d797ad98ad4300c4c6cfc0f800
Ryan Ali <ryanali456@gmail.com>
2024-03-04 15:25:05 +0800

    add email address format validation

--- DeliveryTrackingApp/Areas/Admin/Models/Account.cs
+++ DeliveryTrackingApp/Areas/Admin/Models/Account.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 public class Account {
     public Guid Id {get; set;}
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
     [Required(ErrorMessage = "Email is required.")]
     public string Email {get; set;} = "";
     public string Password {get; set;} = "";
